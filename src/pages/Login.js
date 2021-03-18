c8582ddb3c65c1f0eab164128ad7657e00c4c0ae
gmlwhd6159 <gmlwhd6159@gmail.com>
2021-03-18 14:26:05 +0900

    test Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -10,8 +10,37 @@ class Login extends React.Component{
         this.state = {
             email: null,
             password: null,
-            err: null
+            err: ""
         }
+        this.handleInputValue = this.handleInputValue.bind(this)
+        this.clickLoginhandle = this.clickLoginhandle.bind(this)
+    }
+
+    handleInputValue = (key) => (e) => {
+        this.setState({ [key]: e.target.value });
+       
+      };
+
+    clickLoginhandle(){
+       const {email,password} = this.state
+  
+       if(!email || !password) {
+          this.setState({
+              err : "모든 항목 작성해주세요?"
+          })
+          return ;
+       }
+       else {
+           this.setState({
+               err : ""
+           })
+       }
+
+      return axios.post("url",{email:email, password:password},{withCredentials :true})
+       .then(()=>{
+           //제대로 전달됫으면 어찌 받을까요
+       })
+
     }
 
     render(){
@@ -22,20 +51,21 @@ class Login extends React.Component{
                     <form>
                         <div>
                             <span>Email </span>
-                            <input type="email"></input>
+                            <input type="email" onChange = {this.handleInputValue("email")}></input>
                         </div>
                         <div>
                             <span>Password </span>
-                            <input type="password"></input>
+                            <input type="password" onChange ={this.handleInputValue("password")}></input>
                         </div>
                         <div>
                             <Link to="/signup">아이디가 없으신가요?</Link>
                         </div>
                         <div>
-                            <button>LogIn</button>
+                            <button onClick={this.clickLoginhandle}>LogIn</button>
                         </div>
+                         {this.state.err ? <div  className="alert-box" >{this.state.err}</div> : ""} 
+                       
                         
-                        <div className="alert-error"></div>
                     </form>
                 </center>
             </div>
