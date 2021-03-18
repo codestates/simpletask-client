81d23f8b226337132b7cf07db9d49b78396be678
yhl88 <yhl898989@gmail.com>
2021-03-18 11:01:09 +0900

    change parameter  name

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {Link, Route, BrowserRouter as Router, Switch} from "react-router-dom";
 
 import MainPage from "./pages/MainPage"
 import Login from "./pages/Login"
+import Signup from "./pages/Signup"
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Switch>
           <Route exact path="/" render={() => <MainPage></MainPage>}></Route>
           <Route exact path="/login" render={() => <Login></Login>}></Route>
+          <Route exact path="/signup" render={() => <Signup></Signup>}></Route>
         </Switch>
       </main>
     </Router>
