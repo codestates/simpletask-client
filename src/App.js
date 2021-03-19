486da061587e4c6783323250b3e57571bcbe260a
yhl88 <yhl898989@gmail.com>
2021-03-19 10:32:13 +0900

    change App

--- src/App.js
+++ src/App.js
@@ -2,45 +2,52 @@ import logo from './logo.svg';
 import './App.css';
 import React from "react";
 import {Link, Route, BrowserRouter as Router, Switch} from "react-router-dom";
-
 import MainPage from "./pages/MainPage"
 import Login from "./pages/Login"
 import Signup from "./pages/Signup"
 import Mypage from "./pages/Mypage"
 import Edit from "./pages/Edit"
-
+import axios from 'axios';
 class App extends React.Component{
   constructor(props){
     super(props)
-
     this.state = {
       isLogin: false,
-      userData: null
+      userData: null,
+      text : null
     }
-
     this.loginHandler = this.loginHandler.bind(this);
     this.logoutHandler = this.logoutHandler.bind(this);
   }
-
   loginHandler(){
-    this.setState({
-      isLogin: true
+    return axios.get("https://simpletask.ga/user/mypage",{withCredentials :true})
+    .then((res)=>{
+      this.setState({
+        isLogin:true,
+        userdata: res.data
+      })
+    })
+    .then(()=>{
+      axios.get("https://simpletask.ga/",{withCredentials: true})
+    })
+    .then((tex)=>{
+      this.setState({
+        text : tex.data
+      })
     })
   }
-
   logoutHandler(){
     this.setState({
       isLogin: false
     })
   }
-
   render(){
     return (
       <Router>
         <main>
           <Switch>
             <Route exact path="/" render={() => <MainPage></MainPage>}></Route>
-            <Route exact path="/login" render={() => <Login></Login>}></Route>
+            <Route exact path="/login" render={() => <Login loginHandler = {this.loginHandler}></Login>}></Route>
             <Route exact path="/signup" render={() => <Signup></Signup>}></Route>
             <Route exact path="/mypage" render={() => <Mypage></Mypage>}></Route>
             <Route exact path="/editpassword" render={() => <Edit></Edit>}></Route>
@@ -49,7 +56,5 @@ class App extends React.Component{
       </Router> 
     );
   }
-  
 }
-
 export default App;
