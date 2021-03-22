f6af7a01cc4562edde691434b9a90d36f24e6051
yhl88 <yhl898989@gmail.com>
2021-03-23 02:31:38 +0900

    new text write

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Signup from "./pages/Signup"
 import Mypage from "./pages/Mypage"
 import Edit from "./pages/Edit"
 import axios from "axios";
+import WriteForm from "./pages/WriteForm";
+
 // import axios from 'axios';
 class App extends React.Component{
   constructor(props){
@@ -20,6 +22,8 @@ class App extends React.Component{
     this.logoutHandler = this.logoutHandler.bind(this);
     // this.textHandler = this.textHandler.bind(this)
     this.nomemberLoginHandler = this.nomemberLoginHandler.bind(this);
+    this.deleteHand = this.deleteHand.bind(this);
+    this.HandleTextDelete = this.HandleTextDelete.bind(this);
   }
 // 앱 실행되면 전체 컨텐츠 목록 받아오기
   componentDidMount(){
@@ -56,10 +60,40 @@ class App extends React.Component{
     console.log(this.state.isLogin, this.state.userData,"@@@@@@@@state");
    })
   }
+  // 비회원 로그인 
   nomemberLoginHandler(){
     this.setState({
       isLogin: true
     })
+  }
+  //회원탈퇴
+  deleteHand(){
+    console.log("@@@@@@@@삭제@@@@@@@");
+    console.log(this.state.userData);
+   axios.post("http://localhost:8080/deleteid",{email:this.state.userData.email})
+   .then(()=>{
+     console.log("@@@@@@삭제됨@@@@@@@");
+     this.props.history.push('/')
+     console.log(this.state)
+   })
+   .then(()=>{
+    this.setState({
+      isLogin: false,
+      userData : null
+    })
+  })
+  .catch(err=>console.log(err))
+  }
+
+  HandleTextDelete(){
+    console.log("글 삭제 함수 실행");
+    console.log(this.state.text.id)
+    axios.post("http://localhost:8080/delete", {id: this.state.userData.email})
+    .then(() => {
+      console.log('글 삭제 완료');
+      
+    })
+    .catch((err) => console.log(err))
   }
 
   render(){
@@ -67,11 +101,12 @@ class App extends React.Component{
       <Router>
         <main>
           <Switch>
-            <Route exact path="/" render={() => <MainPage isLogin={this.state.isLogin} texts={this.state.text} userData={this.state.userData} logoutHandler={this.logoutHandler}></MainPage>}></Route>
+            <Route exact path="/" render={() => <MainPage isLogin={this.state.isLogin} texts={this.state.text} userData={this.state.userData} logoutHandler={this.logoutHandler} HandleTextDelete={this.HandleTextDelete}></MainPage>}></Route>
             <Route exact path="/signin" render={() => <Login loginHandler = {this.loginHandler} textHandler ={this.textHandler} nomemberLoginHandler={this.nomemberLoginHandler}></Login>}></Route>
             <Route exact path="/signup" render={() => <Signup></Signup>}></Route>
-            <Route exact path="/mypage" render={() => <Mypage userData={this.state.userData} ViewEdit={this.ViewEdit}></Mypage>}></Route>
+            <Route exact path="/mypage" render={() => <Mypage isLogin={this.state.isLogin} userData={this.state.userData} ViewEdit={this.ViewEdit} deleteHand={this.deleteHand} ></Mypage>}></Route>
             <Route exact path="/editpassword" render={() => <Edit userData={this.state.userData}></Edit>}></Route>
+            <Route exact path="/writeform" render={()=> <WriteForm userData={this.state.userData} componentDidMount={this.componentDidMount}></WriteForm>}></Route>
           </Switch>
         </main>
       </Router> 
